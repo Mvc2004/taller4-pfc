834536c8b96c68efc8da39549e35b07324a276ff
sfcg2 <castillo.sofia@correounivalle.edu.co>
2024-06-03 17:25:43 -0500

    advertencias arregladas, faltan por los test

--- app/src/main/scala/taller4/Newton.scala
+++ app/src/main/scala/taller4/Newton.scala
@@ -1,6 +1,6 @@
 package taller4
 
-import scala.annotation.tailrec
+
 
 sealed trait Expr
 case class Numero(d: Double) extends Expr
@@ -14,28 +14,28 @@ case class Logaritmo(e1: Expr) extends Expr
 
 class Newton {
 
-  type Funcion = Double => Double
+  //type funcion = Double => Double
 
-  def evaluar(f: Expr, a: Atomo, v: Double): Double = f match {
+  private def evaluar(f: Expr, a: Atomo, v: Double): Double = f match {
     case Numero(d) => d
     case Atomo(x) => if (x == a.x) v else throw new IllegalArgumentException(s"Átomo no asignado: $x")
     case Suma(e1, e2) => evaluar(e1, a, v) + evaluar(e2, a, v)
     case Prod(e1, e2) => evaluar(e1, a, v) * evaluar(e2, a, v)
     case Resta(e1, e2) => evaluar(e1, a, v) - evaluar(e2, a, v)
-    case Div(e1, e2) => {
+    case Div(e1, e2) =>
       val denom = evaluar(e2, a, v)
       if (denom == 0) throw new ArithmeticException("División por cero")
       evaluar(e1, a, v) / denom
-    }
+
     case Expo(e1, e2) => Math.pow(evaluar(e1, a, v), evaluar(e2, a, v))
-    case Logaritmo(e1) => {
+    case Logaritmo(e1) =>
       val arg = evaluar(e1, a, v)
       if (arg <= 0) throw new ArithmeticException("Logaritmo de número no positivo")
       Math.log(arg)
-    }
+
   }
 
-  def derivar(f: Expr, a: Atomo): Expr = f match {
+  private def derivar(f: Expr, a: Atomo): Expr = f match {
     case Numero(_) => Numero(0.0)
     case Atomo(x) => if (x == a.x) Numero(1.0) else Numero(0.0)
     case Suma(e1, e2) => Suma(derivar(e1, a), derivar(e2, a))
@@ -47,10 +47,12 @@ class Newton {
   }
 
   def newton(f: Expr, a: Atomo, x0: Double, epsilon: Double, maxIter: Int = 100): Double = {
+
     def buenAprox(x: Double): Boolean = Math.abs(evaluar(f, a, x)) < epsilon
 
     var xi = x0
     var iter = 0
+
     while (!buenAprox(xi) && iter < maxIter) {
       xi = xi - evaluar(f, a, xi) / evaluar(derivar(f, a), a, xi)
       iter += 1
@@ -70,7 +72,7 @@ class Newton {
     case Logaritmo(e1) => s"log(${mostrar(e1)})"
   }
 
-  def limpiar(f: Expr): Expr = f match {
+  private def limpiar(f: Expr): Expr = f match {
     case Suma(e1, Numero(0.0)) => limpiar(e1)
     case Suma(Numero(0.0), e2) => limpiar(e2)
     case Resta(e1, Numero(0.0)) => limpiar(e1)
